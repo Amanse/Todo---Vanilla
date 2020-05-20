4dd861d7aaf0325e4c6bdea8ef8ef4e1f0a08786
Aman setia <amansetiarjp@gmail.com>
2020-05-20 18:35:31 +0530

    Added edit by clicking title

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-//Init variables
-let editMode = false;
-
 //Get tasks from local storage on page load
 getTasks();
 
@@ -17,6 +14,7 @@ document.getElementById("form").addEventListener(
       id: lastId + 1,
       title: taskTitle,
       completed: false,
+      editMode: false,
     };
     oldTasks.push(task);
     localStorage.setItem("tasks", JSON.stringify(oldTasks));
@@ -52,10 +50,35 @@ function deleteTask(id) {
 
 //TODO
 //Turn on edit mode and change form
-function editModeOn() {}
+function editModeOn(el) {
+  let taskList = JSON.parse(localStorage.getItem("tasks"));
+  console.log(el.getAttribute("value"));
+  taskList.forEach((task) => {
+    if (task.id == el.getAttribute("value")) {
+      task.editMode = true;
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+  getTasks();
+}
+
+//TODO
+//Change Task
+function changeTask(id) {
+  let taskList = JSON.parse(localStorage.getItem("tasks"));
+  taskList.forEach((task) => {
+    if (task.id == id) {
+      task.title = document.getElementById("newTitle").value;
+      task.editMode = false;
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+  getTasks();
+}
 
 //Get tasks from local storage and show them, also contains the checkbox and edit title and ofc deleteTask, to be ran at page load
 function getTasks() {
+  document.getElementById("task").value = "";
   var tasks = "";
   if (localStorage.getItem("tasks") == null) {
     localStorage.setItem("tasks", "[]");
@@ -67,6 +90,12 @@ function getTasks() {
   tasks.forEach(function (task, index) {
     let completedTask = "";
     let checked = "";
+    let taskForm =
+      "<span value='" +
+      task.id +
+      "' onclick='editModeOn(this)'>" +
+      task.title +
+      "</span>";
     if (task.completed) {
       completedTask = "checked";
       checked = "text-decoration: line-through; opacity: 25%; color: #111;";
@@ -74,14 +103,24 @@ function getTasks() {
       completedTask = "";
       checked = "";
     }
+
+    if (task.editMode) {
+      taskForm =
+        "<input id='newTitle' class='editTask' onfocusout='changeTask(" +
+        task.id +
+        ")' value='" +
+        task.title +
+        "'>";
+    }
+
     document.getElementById("taskShow").innerHTML +=
       "<h2 style='" +
       checked +
       "'><span onclick='deleteTask(" +
       task.id +
-      ")' class='delete'>X</span><span onclick='editModeOn()'>" +
-      task.title +
-      "</span><input type='checkbox' value='" +
+      ")' class='delete'>X</span>" +
+      taskForm +
+      "<input type='checkbox' value='" +
       task.id +
       "' id='checkbox'" +
       completedTask +
